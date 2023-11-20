5cddf5b19a645eaf8c07ba0d98119d8feb78983a
mah0x211 <mah0x211@gmail.com>
2023-11-20 09:00:00 +0900

    Add RowDescription message decoder

--- lib/message.lua
+++ lib/message.lua
@@ -56,6 +56,7 @@ local DECODER = {
     S = require('postgres.message.parameter_status').decode,
     ['1'] = require('postgres.message.parse_complete').decode,
     Z = require('postgres.message.ready_for_query').decode,
+    T = require('postgres.message.row_description').decode,
 }
 
 --- decode_message
