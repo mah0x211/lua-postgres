568d9646f749207a9892cb83caa4ce870e4d65a1
mah0x211 <mah0x211@gmail.com>
2023-11-20 09:56:08 +0900

    Add CancelRequest message encoder

--- lib/message.lua
+++ lib/message.lua
@@ -79,6 +79,7 @@ end
 return {
     encode = {
         bind = require('postgres.message.bind').encode,
+        cancel_request = require('postgres.message.cancel_request').encode,
         close_portal = require('postgres.message.close_portal').encode,
         close_statement = require('postgres.message.close_statement').encode,
         describe_portal = require('postgres.message.describe_portal').encode,
